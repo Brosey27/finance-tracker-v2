3e905d78724eba124c259892e28724ce66ecba73
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-09-01 19:55:09 +1000

    Add firebase expenses section. Add functionality to add expenses and add income. Installed uuid. Add finance-context and moved firebase import db imports to this file. Project still needs authentication and security.

--- app/page.js
+++ app/page.js
@@ -1,114 +1,110 @@
 "use client";
-import {useState} from 'react'
+
+import { useState, useContext, useEffect } from "react";
+import { financeContext } from "@/lib/store/finance-context";
+
 import { currencyFormatter } from "@/lib/utils";
+
 import ExpenseCategoryItem from "@/components/ExpenseCategoryItem";
-import Modal from "@/components/Modal";
+
+import AddIncomeModal from "@/components/modals/AddincomeModal";
+import AddExpensesModal from "@/components/modals/AddExpensesModal";
 
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-
-const DUMMY_DATA = [
-  {
-    id: 1,
-    title: "Entertainment",
-    color: "#000",
-    total: 500,
-  },
-  {
-    id: 2,
-    title: "Gass",
-    color: "#009",
-    total: 200,
-  },
-  {
-    id: 3,
-    title: "Fuel",
-    color: "#000",
-    total: 1200,
-  },
-  {
-    id: 4,
-    title: "Movies",
-    color: "#000",
-    total: 800,
-  },
-  {
-    id: 5,
-    title: "Holiday",
-    color: "#000",
-    total: 2000,
-  },
-];
-
 export default function Home() {
+  const [showAddIncomeModal, setShowAddIncomeModal] = useState(false);
+  const [showAddExpenseModal, setShowAddExpenseModal] = useState(false);
 
-  const [modalIsOpen, setModalIsOpen] = useState(true);
+  const [balance, setBalance] = useState(0);
+
+  const { expenses, income } = useContext(financeContext);
+
+  useEffect(() => {
+    const newBalance =
+      income.reduce((total, i) => {
+        return total + i.amount;
+      }, 0) -
+      expenses.reduce((total, e) => {
+        return total + e.total;
+      }, 0);
+
+    setBalance(newBalance);
+  }, [expenses, income]);
 
   return (
     <>
-   {/* Modal */}
-   <Modal show={modalIsOpen} onClose={setModalIsOpen}>
-        <h3>Hello World</h3>
-      </Modal>
-   
-    <main className="container max-w-2xl px-6 mx-auto">
- <section className="py-3">
-   <small className="text-gray-400 text-md">My Balance</small>
-   <h2 class="text-4xl font-bold">{ currencyFormatter(1000000)}</h2>
- </section>
+      {/* Add Income Modal */}
+      <AddIncomeModal
+        show={showAddIncomeModal}
+        onClose={setShowAddIncomeModal}
+      />
 
- <section className="flex items-center gap-2 py-3">
- <button
+      {/* Add Expenses Modal */}
+      <AddExpensesModal
+        show={showAddExpenseModal}
+        onClose={setShowAddExpenseModal}
+      />
+
+      <main className="container max-w-2xl px-6 mx-auto">
+        <section className="py-3">
+          <small className="text-gray-400 text-md">My Balance</small>
+          <h2 className="text-4xl font-bold">{currencyFormatter(balance)}</h2>
+        </section>
+
+        <section className="flex items-center gap-2 py-3">
+          <button
             onClick={() => {
-              setModalIsOpen(true);
+              setShowAddExpenseModal(true);
             }}
             className="btn btn-primary"
           >
             + Expenses
           </button>
-        <button className="btn btn-primary-outline">+ Income</button>
- </section>
-
-  {/* Expenses */}
-  <section className="py-6">
-        <h3 className="text-2xl">My Expenses</h3>
-        <div className="flex flex-col gap-4 mt-6">
-          {DUMMY_DATA.map((expense) => {
-            return (
-              <ExpenseCategoryItem
-                color={expense.color}
-                title={expense.title}
-                total={expense.total}
-              />
-            );
-          })}
-        </div>
-      </section>
-
-      {/* Chart Section */}
-      <section className="py-6">
-        <h3 className="text-2xl">Stats</h3>
-        <div className="w-1/2 mx-auto">
-          <Doughnut
-            data={{
-              labels: DUMMY_DATA.map((expense) => expense.title),
-              datasets: [
-                {
-                  label: "Expenses",
-                  data: DUMMY_DATA.map((expense) => expense.total),
-                  backgroundColor: DUMMY_DATA.map((expense) => expense.color),
-                  borderColor: ["#18181b"],
-                  borderWidth: 5,
-                },
-              ],
+          <button
+            onClick={() => {
+              setShowAddIncomeModal(true);
             }}
-          />
-        </div>
-      </section>
- </main>
- </>
-  )
+            className="btn btn-primary-outline"
+          >
+            + Income
+          </button>
+        </section>
+
+        {/* Expenses */}
+        <section className="py-6">
+          <h3 className="text-2xl">My Expenses</h3>
+          <div className="flex flex-col gap-4 mt-6">
+            {expenses.map((expense) => {
+              return <ExpenseCategoryItem key={expense.id} expense={expense} />;
+            })}
+          </div>
+        </section>
+
+        {/* Chart Section */}
+        <section className="py-6">
+          <h3 className="text-2xl">Stats</h3>
+          <div className="w-1/2 mx-auto">
+            <Doughnut
+              data={{
+                labels: expenses.map((expense) => expense.title),
+                datasets: [
+                  {
+                    label: "Expenses",
+                    data: expenses.map((expense) => expense.total),
+                    backgroundColor: expenses.map((expense) => expense.color),
+                    borderColor: ["#18181b"],
+                    borderWidth: 5,
+                  },
+                ],
+              }}
+            />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
