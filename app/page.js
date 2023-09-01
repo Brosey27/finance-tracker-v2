42cc804577453a4d2e11dad3d95ef7b003f98162
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-09-01 12:36:57 +1000

    Add Expenses section to page.js. Add ExpenseCategoryItem to components. Add Dummy_Data for 5 new expenses. Utilised Dummy data with the expensecategoryitem function to finish the expenses section

--- app/page.js
+++ app/page.js
@@ -1,5 +1,40 @@
 "use client";
 import { currencyFormatter } from "@/lib/utils";
+import ExpenseCategoryItem from "@/components/ExpenseCategoryItem";
+
+
+const DUMMY_DATA = [
+  {
+    id: 1,
+    title: "Entertainment",
+    color: "#000",
+    total: 500,
+  },
+  {
+    id: 2,
+    title: "Gass",
+    color: "#009",
+    total: 200,
+  },
+  {
+    id: 3,
+    title: "Fuel",
+    color: "#000",
+    total: 1200,
+  },
+  {
+    id: 4,
+    title: "Movies",
+    color: "#000",
+    total: 800,
+  },
+  {
+    id: 5,
+    title: "Holiday",
+    color: "#000",
+    total: 2000,
+  },
+];
 
 export default function Home() {
   return (
@@ -13,6 +48,22 @@ export default function Home() {
         <button className="btn btn-primary">+ Expenses</button>
         <button className="btn btn-primary-outline">+ Income</button>
  </section>
+
+  {/* Expenses */}
+  <section className="py-6">
+        <h3 className="text-2xl">My Expenses</h3>
+        <div className="flex flex-col gap-4 mt-6">
+          {DUMMY_DATA.map((expense) => {
+            return (
+              <ExpenseCategoryItem
+                color={expense.color}
+                title={expense.title}
+                total={expense.total}
+              />
+            );
+          })}
+        </div>
+      </section>
  </main>
   )
 }
