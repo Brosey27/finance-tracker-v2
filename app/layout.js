e6fd76687c37e8cae35315b0c2ad3283b6de5981
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-09-01 21:44:10 +1000

    Add auth-context. created sign in section, updadted navigation, firebase index.js and layout.js accordingly.

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,27 @@
-import './globals.css'
-import Nav from '@/components/Navigation'
-import FinanceContextProvider from "@/lib/store/finance-context";
+"use client";
 
+import "./globals.css";
+
+import Nav from "@/components/Navigation";
+
+import FinanceContextProvider from "@/lib/store/finance-context";
+import AuthContextProvider from "@/lib/store/auth-context";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-       <head />
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
       <body>
-      <FinanceContextProvider>
-          <Nav />
-          {children}
-        </FinanceContextProvider>
+        <AuthContextProvider>
+          <FinanceContextProvider>
+            <Nav />
+            {children}
+          </FinanceContextProvider>
+        </AuthContextProvider>
       </body>
     </html>
   );
