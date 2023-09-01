b740386b30dd9b6472bb2f692def2b03fce61778
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-09-01 12:26:10 +1000

    Add lib folder with the file utils.js to hold the code for currency formatter. Imported currency formatter in page.js. Edited h2 element to use the currency formatter function. Add buttons for expenses and income and appropriate styling via globals.css

--- app/page.js
+++ app/page.js
@@ -1,7 +1,18 @@
-
+"use client";
+import { currencyFormatter } from "@/lib/utils";
 
 export default function Home() {
   return (
- <h1>Hello</h1>
+    <main className="container max-w-2xl px-6 mx-auto">
+ <section className="py-3">
+   <small className="text-gray-400 text-md">My Balance</small>
+   <h2 class="text-4xl font-bold">{ currencyFormatter(1000000)}</h2>
+ </section>
+
+ <section className="flex items-center gap-2 py-3">
+        <button className="btn btn-primary">+ Expenses</button>
+        <button className="btn btn-primary-outline">+ Income</button>
+ </section>
+ </main>
   )
 }
